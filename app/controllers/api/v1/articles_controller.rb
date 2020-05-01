8a8e7fd81a173a09b51005e88971ca5cf4ff4db6
KoinoEngineering <email@koino.engineering>
2020-05-01 18:02:19 +0900

    #1 fix return type

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            render json: { status:Constants::SUCCESS, data: @article }
+            render json: { status:Constants::SUCCESS, data: [ @article ] }
         else
             render json: { status:Constants::FAILED }
         end
@@ -15,16 +15,16 @@ class Api::V1::ArticlesController < ApplicationController
     def update
         @article = Article.find(params[:id])
         if @article.update(article_params)
-            render json: { status:Constants::SUCCESS, data: @article }
+            render json: { status:Constants::SUCCESS, data: [ @article ] }
         else
-            render json: { status:Constants::FAILED, data: @article }
+            render json: { status:Constants::FAILED, data: [ @article ] }
         end
     end
 
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
-        render json: { status:Constants::SUCCESS, data: @article }
+        render json: { status:Constants::SUCCESS, data: [ @article ] }
     end
 
     private
