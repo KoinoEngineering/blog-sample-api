90fa46f1d6d281cc403f3e0cf2cb23965e5a6d01
KoinoEngineering <email@koino.engineering>
2020-05-01 14:43:35 +0900

    #1 remove node

--- src/interfaces/State.ts
+++
@@ -1,9 +0,0 @@
-import { RouterState } from "connected-react-router";
-import { CreateState } from "src/pages/Create/CreateReducer";
-import { ArticlesState } from "src/modules/db/Articles/ArticlesReducer";
-
-export interface State {
-    articles: ArticlesState;
-    router: RouterState;
-    create: CreateState;
-}
